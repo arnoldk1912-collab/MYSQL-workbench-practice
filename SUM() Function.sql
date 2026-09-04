7cae6716385ff176f9f8f67e6c69d018139fcc15
Arnold kyro <arnoldk1912@gmail.com>
2026-09-05 00:58:19 +0400

    SUM() with expressions practice

--- SUM() Function.sql
+++ SUM() Function.sql
@@ -1,3 +1,6 @@
+# The SQL SUM() Function
+# The SUM() function is used to calculate the total sum of values within a numeric column.
+# The SUM() function ignores NULL values in the column.
 SELECT SUM(rental_rate)
 FROM film;
 
@@ -13,3 +16,13 @@ SUM(length) AS Total_length,
 min(length) AS Lowest, MAX(length) AS Higest, rating
 FROM film
 GROUP BY rating;
+
+# SUM() With an Expression
+# The parameter inside the SUM() function can also be an expression.
+SELECT SUM(rental_rate)
+FROM film;
+
+# # Use an expression inside the SUM() function:
+SELECT SUM(rental_rate / 11)
+FROM film;
+
