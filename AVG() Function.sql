1ee44b5cc0eda178646c75bf7599d077a4a71d6f
Arnold kyro <arnoldk1912@gmail.com>
2026-09-09 04:08:20 +0400

    AVG() with subquery pratice

--- AVG() Function.sql
+++ AVG() Function.sql
@@ -25,3 +25,35 @@ MAX(unitprice) AS higest_price,
 SUM(unitprice) AS Total_price, AVG(unitprice) AS Average_price
 FROM Track
 GROUP BY albumid;
+
+SELECT * FROM track
+WHERE unitprice > (SELECT AVG(unitprice) FROM track);
+
+SELECT rental_duration 
+FROM film
+WHERE rental_duration > (SELECT AVG(rental_duration) FROM film);
+
+SELECT AVG(rental_duration)
+FROM film;
+
+SELECT title, length
+FROM film
+WHERE length = (SELECT MAX(length) FROM film);
+
+SELECT title, length
+FROM film
+WHERE length = (SELECT MIN(length) FROM film);
+
+SELECT title, length
+FROM film 
+WHERE length = (SELECT MIN(length) FROM film)
+   OR length = (SELECT MAX(length) FROM film)
+ORDER BY length;
+
+SELECT title, length
+FROM film
+WHERE length IN(
+    (SELECT MIN(length) FROM film),
+    (SELECT MAX(length) FROM film)
+)
+ORDER BY length;
