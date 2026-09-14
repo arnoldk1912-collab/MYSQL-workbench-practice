ae3e8fe6e6d44ffe23b5fbcc1a7061931733107f
Arnold kyro <arnoldk1912@gmail.com>
2026-09-14 04:19:58 +0400

    Day 2: LIKE operator training

--- LIKE Operator training.sql
+++ LIKE Operator training.sql
@@ -15,3 +15,32 @@ WHERE last_name LIKE '%on%';
 
 SELECT * FROM customer
 WHERE customer_id LIKE '%512%';
+
+# The _ Wildcard
+# The _ wildcard represents one single character.
+
+# It can be any character or number, but each _ represents one, and only one, character.
+SELECT * FROM customer
+WHERE first_name LIKE 'Ar__l_';
+
+# To return records that starts with a specific letter or phrase, add the % at the end of the letter or phrase.
+SELECT * FROM customer
+WHERE first_name LIKE 'ma%'
+LIMIT 5;
+
+# We can also combine any number of conditions using AND or OR operators.
+SELECT * FROM customer
+WHERE first_name LIKE 'E%' AND last_name LIKE '%A';
+
+SELECT * FROM customer
+WHERE first_name LIKE 'A_A_' OR last_name LIKE 'E_A';
+
+# You can also combine "starts with" and "ends with":
+# Return all customers that starts with "F" and ends with "K":
+SELECT * FROM customer
+WHERE first_name LIKE 'F%K';
+
+SELECT rating, COUNT(*) AS How_many
+FROM film 
+WHERE title LIKE 'A%'
+GROUP BY rating;
